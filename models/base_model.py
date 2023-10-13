75ebc8b6ee81f22a6e14081e4351936d389c8f97
knwble <noble4peter1@gmail.com>
2023-10-13 22:17:56 +0100

    added dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,26 @@ class BaseModel():
         This class defines all common attributes/methods for other classes
     """
     def __init__(self):
+        """ initializes a new BaseModel """
+
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
+        """ Returns String Representation """
+   
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
+        """ Updates the public instance attribute update_at """
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of __dict__ """
+
+        mydict = self.__dict__.copy()
+        mydict["__class__"] = self.__class__.__name__
+        mydict["created_at"] = self.created_at.isoformat()
+        mydict["updated_at"] = self.updated_at.isoformat()
+        return mydict
