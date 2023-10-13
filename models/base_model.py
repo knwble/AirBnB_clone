5904bc45abcca5893f3e8d66c98ab5f84bebc4c7
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-10-13 12:55:49 -0700

    updated base model class

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from datetime import datetime
-import uuid
+from uuid import uuid4
 
 """ This is the BaseModel file"""
 
@@ -9,7 +9,7 @@ class BaseModel():
         This class defines all common attributes/methods for other classes
     """
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
     
