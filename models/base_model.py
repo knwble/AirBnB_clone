4b0d7eec43abac707ca3a35271b57b27ef8d4535
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-10-14 05:38:16 -0700

    fixed pycodestyle error

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,51 @@
 #!/usr/bin/python3
-from datetime import datetime
-from uuid import uuid4
+"""This python script is the base model"""
 
-""" This is the BaseModel file"""
+import uuid
+from datetime import datetime
+
 
 class BaseModel():
-    """
-        This class defines all common attributes/methods for other classes
-    """
+
+    """This class defines all common attributes/methods for other classes"""
+
     def __init__(self, *args, **kwargs):
         """ initializes a new BaseModel
 
         Args:
-        *args: List of arguments (not used)
-        **kwargs: Dictionary of key-value arguments
+            *args: List of arguments (not used)
+            **kwargs: Dictionary of key-value arguments
 
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ("created_at", "updated_at"):
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
-            self.id = kwargs.get("id", str(uuid4()))
 
+        if kwargs is not None and kwargs != {}::
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["update    d_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def __str__(self):
         """ Returns String Representation """
 
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """ Updates the public instance attribute update_at """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ """
 
         mydict = self.__dict__.copy()
-        mydict["__class__"] = self.__class__.__name__
-        mydict["created_at"] = self.created_at.isoformat()
-        mydict["updated_at"] = self.updated_at.isoformat()
+        mydict["__class__"] = type(self).__name__
+        mydict["created_at"] = mydict["created_at"].isoformat()
+        mydict["updated_at"] = mydict["updated_at"].isoformat()
         return mydict
