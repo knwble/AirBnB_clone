a414ab05b23683d29f16d59b09d92aee59277b2a
chiefebube <ebubechi.anyanwu@yahoo.com>
2023-10-14 05:40:06 -0700

    fixed pycodestyle error

--- models/base_model.py
+++ models/base_model.py
@@ -18,12 +18,14 @@ class BaseModel():
 
         """
 
-        if kwargs is not None and kwargs != {}::
+        if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(kwargs["update    d_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["update    d_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
