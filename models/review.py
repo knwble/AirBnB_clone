32bbea8ba7d5261812b210aeb156af1ab06d6b98
knwble <noble4peter1@gmail.com>
2023-10-16 17:53:49 +0100

    identation fixed

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review 
+    """Class for managing review
     Attributes:
         place_id (str): The Place id.
         user_id (str): The User id.
