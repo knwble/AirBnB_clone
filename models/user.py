b3e6fa79fcd2310ba0287eaf8ca8ef79359de6cf
knwble <noble4peter1@gmail.com>
2023-10-16 05:58:48 +0100

    update

--- models/user.py
+++ models/user.py
@@ -4,7 +4,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class for managing user objects"""
+    """Class for managing user objects
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user."""
 
     email = ""
     password = ""
